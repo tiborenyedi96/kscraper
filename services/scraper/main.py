6c85ee478670090dfbb77b1cb845a96837574b59
Tibor Enyedi <tibor.enyedi1996@gmail.com>
2026-03-25 18:23:16 +0000

    basic scraper implemented for the test website

--- services/scraper/main.py
+++ services/scraper/main.py
@@ -1,30 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
+def scrape(url: str) -> dict[str, str]:
+    page_limiter: int = 100
+    keys: list[str] = []
+    values: list[str] = []
+
+    for i in range(1, page_limiter):
+        current_url: str = f"{url}/catalogue/page-{i}.html"
+        try:
+            request = requests.get(current_url)
+        except Exception as e:
+            print(e)
+            return None
+
+        if request.status_code != 200:
+            break
+
+        soup = BeautifulSoup(request.content.decode("utf-8"), features="html.parser")
+
+        for item in soup.find_all("a"):
+            if item.get("title"):
+                keys.append(item.get("title"))
+
+        for item in soup.find_all("p", {"class": "price_color"}):
+            values.append(item.get_text().replace("£", ""))
+
+    result = dict(zip(keys, values))
+    return result
+
 def main():
-    try:
-        request = requests.get("https://books.toscrape.com/")
-    except Exception as e:
-        print (e)
-        return None
-
-    soup = BeautifulSoup(request.content.decode("utf-8"), features="html.parser")
-    
-    # getting the book titles
-    book_titles: list[str] = []
-    for item in soup.find_all("a"):
-        if item.get("title"):
-            book_titles.append(item.get("title"))
-
-    # getting the book prices
-    book_prices: list[str] = []
-    for item in soup.find_all("p", {"class": "price_color"}):    
-        book_prices.append(item.get_text().replace("£", ""))
-    
-    # merge them together
-    book_dict = dict(zip(book_titles, book_prices))
-
-    print(book_dict)
+    result = scrape("https://books.toscrape.com")
+    print(result)
 
 if __name__ == "__main__":
     main()
