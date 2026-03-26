ed279c6b9489d995efcbce55ad0cc381cb1b2fb1
Tibor Enyedi <tibor.enyedi1996@gmail.com>
2026-03-26 11:09:28 +0000

    fix: improved exception handling in parser.py

--- services/scraper/parser.py
+++ services/scraper/parser.py
@@ -13,12 +13,11 @@ class ScraperConfig(TypedDict):
 def parse_configuration(filepath: str) -> ScraperConfig:
     try:
         with open(filepath, "r") as file:
-            try:
-                data = yaml.safe_load(file)
-            except yaml.YAMLError:
-                raise yaml.YAMLError(f"Yaml file is invalid: {filepath}")
+            data = yaml.safe_load(file)
     except FileNotFoundError:
         raise FileNotFoundError(f"Configuration file not found: {filepath}")
+    except yaml.YAMLError as e:
+        raise yaml.YAMLError(f"Yaml file is invalid: {filepath}") from e
 
     try:
         output: ScraperConfig = {
@@ -27,8 +26,8 @@ def parse_configuration(filepath: str) -> ScraperConfig:
             "limiter": data["site"]["pagination"]["limiter"],
             "fields": data["site"]["fields"],
         }
-    except KeyError:
+    except (KeyError, TypeError) as e:
         raise KeyError(
             "Configuration is missing one or more required fields. (required fields: site, url, pagination, limiter, fields)"
-        )
+        ) from e
     return output
