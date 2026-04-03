173c36450b70efe41d04890f361db580c3ed4233
Tibor Enyedi <tibor.enyedi1996@gmail.com>
2026-04-03 11:13:33 +0000

    feat: added consumer funtion to broker service

--- packages/shared/src/shared/broker.py
+++ packages/shared/src/shared/broker.py
@@ -19,3 +19,31 @@ def send_messages(messages: list[str]) -> None:
     for message in messages:
         channel.basic_publish(exchange="", routing_key="kscraper", body=message)
     connection.close()
+
+
+def receive_messages():
+    credentials = pika.PlainCredentials(
+        os.getenv("RABBITMQ_DEFAULT_USER"),
+        os.getenv("RABBITMQ_DEFAULT_PASS"),
+        erase_on_connect=True,
+    )
+    parameters = pika.ConnectionParameters(
+        host="rabbitmq", virtual_host="kscraper", credentials=credentials
+    )
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(
+        queue="kscraper", durable=True, arguments={"x-queue-type": "quorum"}
+    )
+
+    def callback(ch, method, properties, body):
+        print(body)
+
+    try:
+        channel.basic_consume(
+            queue="kscraper", on_message_callback=callback, auto_ack=True
+        )
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+    connection.close()
